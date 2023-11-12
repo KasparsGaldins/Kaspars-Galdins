2badbd7d60cb3820b903cedf1b6e8517afcea18a
kg <galdins.kasparss@gmail.com>
2023-11-12 16:55:12 +0000

    update api atslegai

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "APDCadgoB8g6YLSSfqzmTaUcn1Ejf4oohflncv3s"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
